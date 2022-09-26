f3af817df313deefb69a6d84c3a947c414ef6829
Tors-0 <lukehammer10@gmail.com>
2022-09-26 08:58:35 -0700

    update

---
+++ src/Client.java
@@ -0,0 +1,6 @@
+public class Client {
+    public static void main(String[] args) {
+        Student rae = new Student("rae",11);
+        System.out.print(rae);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    String name;
+    int grade;
+    public Student(String myName, int myGrade) {
+        name = myName;
+        grade = myGrade;
+    }
+}
