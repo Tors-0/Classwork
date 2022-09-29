4b9dacc883fe969692d8c35498b68c198f007930
Tors-0 <lukehammer10@gmail.com>
2022-09-29 09:23:44 -0700

    2022.9.29-1

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,7 @@
-import java.util.Scanner;
-
 public class Client {
-    
+    public static void main(String[] args) {
+
+
+        new WindowInit().run();
+    }
 }
---
+++ src/Voxel.java
@@ -0,0 +1,12 @@
+public class Voxel {
+    public final int xCord;
+    public final int yCord;
+    public final int zCord;
+    public int hexColor;
+    public Voxel(int x, int y, int z, int hex) {
+        xCord = x;
+        yCord = y;
+        zCord = z;
+        hexColor = hex;
+    }
+}
