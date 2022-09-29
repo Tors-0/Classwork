01c494403c7e496aeb5329987ff8f3b1107e4c23
Tors-0 <lukehammer10@gmail.com>
2022-09-29 08:04:35 -0700

    2022.9.29-0

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,5 @@
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number to be parsed: ");
-        long num = input.nextLong();
-        System.out.println(Functions.modChallengeLoop(num));
-    }
+    
 }
--- src/Testing.java
+++ src/Testing.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Testing {
     public static void main(String[] args) {
-        double x = 3;
-        int y = (int) x / 2;
-        System.out.println(x + "\n" + y);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number to be parsed: ");
+        long number = Long.parseLong(input.nextLine());
+        System.out.println(Functions.modChallengeLoop(number));
     }
 }
