d841cdf48577163c67626b0a1f51b8ea8c466adf
Tors-0 <lukehammer10@gmail.com>
2022-09-29 09:30:09 -0700

    2022.9.29-2

---
+++ src/Error.java
@@ -0,0 +1,9 @@
+public class Error {
+    public static void main(String[] args) {
+        long x = 0;
+        while (true) {
+            x += 1;
+            System.out.println(x);
+        }
+    }
+}
