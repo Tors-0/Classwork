94f0e811f472c18d36687be42fca2a2dd02634e5
Tors-0 <lukehammer10@gmail.com>
2022-10-13 09:23:07 -0700

    2022.10.13-2

--- src/Unit2Project.java
+++ src/Unit2Project.java
@@ -1,10 +1,35 @@
 import com.birdbraintechnologies.*;
 
+import java.util.Scanner;
+
 public class Unit2Project {
     public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
         Finch myFinch = new Finch();
-        myFinch.saySomething("Hello!");
-        myFinch.sleep(3000);
+        //System.out.print("What is your name: ");
+        //String speech = input.nextLine();
+        //String firstName = speech.substring(0,speech.indexOf(" "));
+        //String lastName = speech.substring(speech.indexOf(" "));
+        // "Hello! I am Finch and I can't speak through the robot, so I use the computer."
+        //myFinch.saySomething("Hello there " + firstName + ", " + lastName);
+        int time = 7000;
+        //myFinch.sleep(time);
+        System.out.print("What speed: ");
+        //int speed = input.nextInt();
+        System.out.print("How long: ");
+        //time = input.nextInt();
+        time = 850;
+        int speed = 75;
+        int turnTime = 650;
+        myFinch.saySomething("Moving at " + speed + " for " + time);
+        myFinch.setWheelVelocities(speed,speed,time);
+        myFinch.setWheelVelocities(100,-100,turnTime);
+        myFinch.setWheelVelocities(75,75,850);
+        myFinch.setWheelVelocities(100,-100,turnTime);
+        myFinch.setWheelVelocities(speed,speed,time);
+        myFinch.setWheelVelocities(100,-100,turnTime);
+        myFinch.setWheelVelocities(75,75,850);
+        myFinch.setWheelVelocities(100,-100,turnTime);
         myFinch.quit();
     }
 }
