26192cc8a30a19f0809a0acd3c5620bbb6a56067
rae <lukehammer104@gmail.com>
2022-10-24 22:17:53 -0700

    2022.10.24-1

--- src/Testing.java
+++ src/Testing.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
-
 public class Testing {
+    public static boolean array123(int[] nums) {
+        for (int i = 0; i < (nums.length - 2); i++) {
+            if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number to be parsed: ");
-        long number = Long.parseLong(input.nextLine());
-        System.out.println(Functions.modChallengeLoop(number));
+        int[] nums = {1,1,2,1,1};
+        System.out.println(array123(nums));
     }
 }
