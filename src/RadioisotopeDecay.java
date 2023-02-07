ac68b03c726d68ca1ae901cee58a329fd0468411
Raeeeee <73916067+Tors-0@users.noreply.github.com>
2023-02-07 21:56:54 +0000

    2023.02.07-1

---
+++ src/RadioisotopeDecay.java
@@ -0,0 +1,66 @@
+public static Class RadioisotopeDecay{
+    static RadioParticle[] sample = new RadioParticle[100];
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            sample[i] = new RadioParticle();
+        }
+        while (checkIfAnyAlive()) {
+            if (checkIfAnyAlive()) {
+                ageAllUp();
+            }
+        }
+        summary();
+    }
+    public static void ageAllUp() {
+        for (int i = 0; i < 100; i++) {
+            sample[i].ageUp();
+        }
+    }
+    public static boolean checkIfAnyAlive() {
+        int aliveCount = 0;
+        for (int i = 0; i < 100; i++) {
+            if (sample[i].isAlive()) {
+                aliveCount++;
+            }
+        }
+        if (aliveCount > 0) return true;
+        else return false;
+    }
+    public static void summary() {
+        int totalSurvivedCount = 0;
+        int maxAge = 0;
+        for (int i = 0; i < 100; i++) {
+            totalSurvivedCount += sample[i].getSurvivedCount();
+            if (sample[i].age >= maxAge) {
+                maxAge = sample[i].age;
+            }
+        }
+        double avgSurvived = totalSurvivedCount / 100;
+        System.out.println("Maximum Age Reached: " + maxAge + "\nAverage Age Reached: " avgSurvived);
+    }
+}
+Class RadioParticle() {
+    protected int age = 0;
+    private boolean alive = true;
+    private int survivedCount = 0;
+    public RadioParticle() {
+        this.age = 0;
+        this.alive = true;
+        this.survivedCount = 0;
+    }
+    public void ageUp() {
+        this.age += 1;
+        if (this.alive) {
+            if (Math.random() > 0.5) {
+                this.alive = false;
+            }
+            this.survivedCount++;
+        }
+    }
+    public boolean isAlive() {
+        return this.alive;
+    }
+    public int getSurvivedCount() {
+        return this.survivedCount;
+    }
+}
