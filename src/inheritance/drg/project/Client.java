4a45de6c6abcfc5f628f27c89bad9725460465a0
rae <lukehammer104@gmail.com>
2023-03-30 19:27:23 -0700

    2023.03.30-2

--- src/inheritance/drg/project/Client.java
+++ src/inheritance/drg/project/Client.java
@@ -1,5 +1,59 @@
 package inheritance.drg.project;
 
-public class Client {
+import java.util.Scanner;
 
+public class Client {
+        static int playerCount;
+        static int hazardLevel;
+        static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        selectDifficulty();
+        GlyphidGrunt notSteeve = createEnemy();
+
+    }
+    public static void selectDifficulty() {
+        System.out.print("Select your mission difficulty (1-5): ");
+        hazardLevel = in.nextInt();
+        if (hazardLevel >= 1 && 5 >= hazardLevel) {
+            System.out.print("\sHow many dwarves are on this mission? (1-4): ");
+            playerCount = in.nextInt();
+            if (!(1 <= playerCount && playerCount <= 4)) {
+                System.out.println("Invalid choice, try again...\n");
+                selectDifficulty();
+            }
+        } else {
+            System.out.println("Invalid choice, try again...\n");
+            selectDifficulty();
+        }
+    }
+    public static GlyphidGrunt createEnemy() {
+        double health = 0;
+        switch (hazardLevel) {
+            case 1:
+                health = 63;
+            case 2:
+                health = 90;
+            case 3:
+                health = 99;
+            case 4, 5:
+                health = 108;
+        }
+        double damage = 0;
+        switch (hazardLevel) {
+            case 1:
+                damage = 2;
+            case 2:
+                damage = 2.4 + (0.4 * playerCount);
+            case 3:
+                damage = 4.4 + (0.4 * playerCount);
+            case 4:
+                if (playerCount == 4) damage = 10;
+                else {
+                    damage = 7.4 + (0.6 * playerCount);
+                }
+            case 5:
+                damage = 10.4 + (0.8 * playerCount);
+        }
+        return new GlyphidGrunt(health,0,health,damage);
+    }
 }
--- src/inheritance/drg/project/GlyphidGrunt.java
+++ src/inheritance/drg/project/GlyphidGrunt.java
@@ -2,8 +2,52 @@ package inheritance.drg.project;
 
 public class GlyphidGrunt extends Glyphid {
     private boolean isSteeve = false; // glyphid grunts can be tamed to become Steeve, who is friendly
+
+    /**
+     * Creates a customizable Grunt
+     * @param h spawning health
+     * @param t temperature (-30 is freezing, 30 is burning)
+     * @param mH max possible health
+     * @param d damage dealt per attack
+     */
     public GlyphidGrunt(double h, double t, double mH, double d) {
         super(h, t, mH, d);
+    }
+
+    /**
+     *
+     * @param hazardLevel
+     * @param playerCount
+     */
+    public GlyphidGrunt(int hazardLevel, int playerCount) {
+        double health = 0;
+        switch (hazardLevel) {
+            case 1:
+                health = 63;
+            case 2:
+                health = 90;
+            case 3:
+                health = 99;
+            case 4, 5:
+                health = 108;
+        }
+        double damage = 0;
+        switch (hazardLevel) {
+            case 1:
+                damage = 2;
+            case 2:
+                damage = 2.4 + (0.4 * playerCount);
+            case 3:
+                damage = 4.4 + (0.4 * playerCount);
+            case 4:
+                if (playerCount == 4) damage = 10;
+                else {
+                    damage = 7.4 + (0.6 * playerCount);
+                }
+            case 5:
+                damage = 10.4 + (0.8 * playerCount);
+        }
+        this(health,0,health,damage);
     }
 
     /**
