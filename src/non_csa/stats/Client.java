caa60d935cb2ae97166df28ff8b1b5f5ad687a0f
rae <raej0000@gmail.com>
2023-09-23 16:42:02 -0700

    refining

--- src/non_csa/stats/Client.java
+++ src/non_csa/stats/Client.java
@@ -5,7 +5,10 @@ import java.util.DoubleSummaryStatistics;
 
 public class Client {
     public static void main(String[] args) {
-        double[] arr = {62,69,36,34,50,16,54,98,28,9,45,41,100,18,78,84,70,56,6,88,35};
+        double[] arr = new double[150_000_000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.round((Math.random() * 10) * 100) / 100d;
+        }
         ArrayStats stat = new ArrayStats(arr);
         System.out.println("5 number summary:\nmin: " + stat.min());
         System.out.println("q1: " + stat.q1());
@@ -15,6 +18,6 @@ public class Client {
         System.out.println("\nother bits:\nmean: " + stat.mean());
         System.out.println("iqr: " + stat.iqr());
         System.out.println("n: " + stat.n());
-        System.out.println("data: " + stat.data());
+        System.out.println("data: " + "stat.data()");
     }
 }
